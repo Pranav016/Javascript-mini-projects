aa95c82dcdf94cc269eee530c7e57dc428ee8f1c
Pranav <pranavmendi@gmail.com>
2020-12-10 06:46:41 +0530

    Added sounds and bg-images to buttons

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,7 +1,40 @@
-var drums= document.querySelectorAll(".drum")
+var drums= document.querySelectorAll(".drum");
 
 for(var i=0; i<drums.length;i++){
     drums[i].addEventListener("click",function (){
-        alert("Button clicked")
+        this.style.color="white";
+        var data=this.innerHTML;
+        switch(data){
+            case 'w':
+                var audio= new Audio("sounds/tom-1.mp3");
+                audio.play();
+            break;
+            case 'a':
+                var audio= new Audio("sounds/tom-2.mp3");
+                audio.play();
+            break;
+            case 's':
+                var audio= new Audio("sounds/tom-3.mp3");
+                audio.play();
+            break;
+            case 'd':
+                var audio= new Audio("sounds/tom-4.mp3");
+                audio.play();
+            break;
+            case 'j':
+                var audio= new Audio("sounds/snare.mp3");
+                audio.play();
+            break;
+            case 'k':
+                var audio= new Audio("sounds/kick.mp3");
+                audio.play();
+            break;
+            case 'l':
+                var audio= new Audio("sounds/kick-bass.mp3");
+                audio.play();
+            break;
+            default:
+                console.log(data);
+        }
     })
 }
